798a79fc42911765aa2ab35229d4a8111cf37efc
Lakshmi115-sr <b.a.rao115@gmail.com>
2026-02-03 23:41:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,18 @@
 const CACHE_NAME = 'kotappakonda-v1';
-const MAP_CACHE = 'map-tiles-v1';
-
-const APP_ASSETS = [
-    '/',
-    '/index.html',
-    'https://cdn.tailwindcss.com',
-    'https://unpkg.com/maplibre-gl@latest/dist/maplibre-gl.js',
-    'https://unpkg.com/maplibre-gl@latest/dist/maplibre-gl.css'
-];
-
-// 1. Install Event
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(APP_ASSETS);
-        })
-    );
-    self.skipWaiting();
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['/'])));
 });
 
-// 2. Fetch Event (This saves the map tiles as you view them)
-self.addEventListener('fetch', (event) => {
-    const url = new URL(event.request.url);
-    
-    if (url.hostname.includes('openfreemap.org') || url.hostname.includes('openstreetmap.org')) {
-        event.respondWith(
-            caches.open(MAP_CACHE).then((cache) => {
-                return cache.match(event.request).then((response) => {
-                    return response || fetch(event.request).then((networkResponse) => {
-                        cache.put(event.request, networkResponse.clone());
-                        return networkResponse;
-                    });
-                });
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request).then((response) => {
-                return response || fetch(event.request);
-            })
-        );
-    }
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request).then(response => {
+                if (e.request.url.includes('tiles')) {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+                }
+                return response;
+            });
+        })
+    );
 });
